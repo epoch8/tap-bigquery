eba5e267c8853fa1556fd92760bfd71ef1585fe4
Andrey Tatarinov <a@tatarinov.co>
2022-05-26 18:57:55 +0300

    make it work

--- tap_bigquery/client.py
+++ tap_bigquery/client.py
@@ -3,9 +3,14 @@
 This includes BigQueryStream and BigQueryConnector.
 """
 
+from typing import Any, Dict, Iterable, List, Optional, cast
 import sqlalchemy
 
+import singer
+
 from singer_sdk import SQLConnector, SQLStream
+from singer_sdk import typing as th  # JSON schema typing helpers
+from singer_sdk.helpers._singer import CatalogEntry, MetadataMapping
 
 
 class BigQueryConnector(SQLConnector):
@@ -13,37 +18,114 @@ class BigQueryConnector(SQLConnector):
 
     def get_sqlalchemy_url(cls, config: dict) -> str:
         """Concatenate a SQLAlchemy URL for use in connecting to the source."""
-        # TODO: Replace this with a valid connection string for your source:
-        return (
-            f"awsathena+rest://{config['aws_access_key_id']}:"
-            f"{config['aws_secret_access_key']}@athena"
-            f".{config['aws_region']}.amazonaws.com:443/"
-            f"{config['schema_name']}?"
-            f"s3_staging_dir={config['s3_staging_dir']}"
-        )
+        return f"bigquery://{config['project_id']}"
 
-    @staticmethod
-    def to_jsonschema_type(sql_type: sqlalchemy.types.TypeEngine) -> dict:
-        """Returns a JSON Schema equivalent for the given SQL type.
+    # TODO fix this in `singer_sdk`
+    def create_sqlalchemy_engine(self) -> sqlalchemy.engine.Engine:
+        return sqlalchemy.create_engine(self.sqlalchemy_url, echo=False)
 
-        Developers may optionally add custom logic before calling the default
-        implementation inherited from the base class.
+    # TODO propose modular structure for `discover_catalog_entries` in `singer_sdk`
+    # Extract discover_catalog_entries from base class because of sqlalchemy-bigquery quirk
+    def discover_catalog_entries(self) -> List[dict]:
+        """Return a list of catalog entries from discovery.
+
+        Returns:
+            The discovered catalog entries as a list.
         """
-        # Optionally, add custom logic before calling the super().
-        # You may delete this method if overrides are not needed.
-        return super().to_jsonschema_type(sql_type)
+        result: List[dict] = []
+        engine = self.create_sqlalchemy_engine()
+        inspected = sqlalchemy.inspect(engine)
+        for schema_name in inspected.get_schema_names():
+            # Get list of tables and views
+            table_names = inspected.get_table_names(schema=schema_name)
+            try:
+                view_names = inspected.get_view_names(schema=schema_name)
+            except NotImplementedError:
+                # Some DB providers do not understand 'views'
+                self._warn_no_view_detection()
+                view_names = []
+            object_names = [(t, False) for t in table_names] + [
+                (v, True) for v in view_names
+            ]
 
-    @staticmethod
-    def to_sql_type(jsonschema_type: dict) -> sqlalchemy.types.TypeEngine:
-        """Returns a JSON Schema equivalent for the given SQL type.
-        
-        Developers may optionally add custom logic before calling the default
-        implementation inherited from the base class.
-        """
-        # Optionally, add custom logic before calling the super().
-        # You may delete this method if overrides are not needed.
-        return super().to_sql_type(jsonschema_type)
+            # Iterate through each table and view
+            for table_name, is_view in object_names:
 
+                ######################
+                # Ugly fix for sqlalchemy-bigquery quirk
+                ######################
+                table_name = table_name.split('.')[-1]
+
+                # Initialize unique stream name
+                unique_stream_id = self.get_fully_qualified_name(
+                    db_name=None,
+                    schema_name=schema_name,
+                    table_name=table_name,
+                    delimiter="-",
+                )
+
+                # Detect key properties
+                possible_primary_keys: List[List[str]] = []
+                pk_def = inspected.get_pk_constraint(table_name, schema=schema_name)
+                if pk_def and "constrained_columns" in pk_def:
+                    possible_primary_keys.append(pk_def["constrained_columns"])
+                for index_def in inspected.get_indexes(table_name, schema=schema_name):
+                    if index_def.get("unique", False):
+                        possible_primary_keys.append(index_def["column_names"])
+                key_properties = next(iter(possible_primary_keys), None)
+
+                # Initialize columns list
+                table_schema = th.PropertiesList()
+                for column_def in inspected.get_columns(table_name, schema=schema_name):
+                    column_name = column_def["name"]
+                    is_nullable = column_def.get("nullable", False)
+                    jsonschema_type: dict = self.to_jsonschema_type(
+                        cast(sqlalchemy.types.TypeEngine, column_def["type"])
+                    )
+                    table_schema.append(
+                        th.Property(
+                            name=column_name,
+                            wrapped=th.CustomType(jsonschema_type),
+                            required=not is_nullable,
+                        )
+                    )
+                schema = table_schema.to_dict()
+
+                # Initialize available replication methods
+                addl_replication_methods: List[str] = [""]  # By default an empty list.
+                # Notes regarding replication methods:
+                # - 'INCREMENTAL' replication must be enabled by the user by specifying
+                #   a replication_key value.
+                # - 'LOG_BASED' replication must be enabled by the developer, according
+                #   to source-specific implementation capabilities.
+                replication_method = next(
+                    reversed(["FULL_TABLE"] + addl_replication_methods)
+                )
+
+                # Create the catalog entry object
+                catalog_entry = CatalogEntry(
+                    tap_stream_id=unique_stream_id,
+                    stream=unique_stream_id,
+                    table=table_name,
+                    key_properties=key_properties,
+                    schema=singer.Schema.from_dict(schema),
+                    is_view=is_view,
+                    replication_method=replication_method,
+                    metadata=MetadataMapping.get_standard_metadata(
+                        schema_name=schema_name,
+                        schema=schema,
+                        replication_method=replication_method,
+                        key_properties=key_properties,
+                        valid_replication_keys=None,  # Must be defined by user
+                    ),
+                    database=None,  # Expects single-database context
+                    row_count=None,
+                    stream_alias=None,
+                    replication_key=None,  # Must be defined by user
+                )
+                result.append(catalog_entry.to_dict())
+
+        return result
 
 class BigQueryStream(SQLStream):
     """Stream class for BigQuery streams."""
--- tap_bigquery/tap.py
+++ tap_bigquery/tap.py
@@ -1,6 +1,6 @@
 """BigQuery tap class."""
 
-from typing import List
+from typing import List, Type
 
 from singer_sdk import SQLTap, SQLStream
 from singer_sdk import typing as th  # JSON schema typing helpers
@@ -11,29 +11,13 @@ class TapBigQuery(SQLTap):
     """BigQuery tap class."""
     name = "tap-bigquery"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "auth_token",
+            "project_id",
             th.StringType,
             required=True,
-            description="The token to authenticate against the API service"
-        ),
-        th.Property(
-            "project_ids",
-            th.ArrayType(th.StringType),
-            required=True,
-            description="Project IDs to replicate"
-        ),
-        th.Property(
-            "start_date",
-            th.DateTimeType,
-            description="The earliest record date to sync"
-        ),
-        th.Property(
-            "api_url",
-            th.StringType,
-            default="https://api.mysample.com",
-            description="The url for the API service"
+            description="GCP Project"
         ),
     ).to_dict()
+
+    default_stream_class: Type[SQLStream] = BigQueryStream
